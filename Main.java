c061b3f81d48a29c3bac96b51addb6e50f8c2c79
Kaanznn <yk.uzun1907@gmail.com>
2026-02-25 14:39:28 +0300

    Yahya Kaan Uzun Assignment

--- Main.java
+++ Main.java
@@ -11,6 +11,7 @@ public class Main {
 
         StudentName simple = new StudentName();
 
+
         simple.printNames(student1, student2);
 
     }
---
+++ StudentName.java
@@ -0,0 +1,15 @@
+package tr.edu.beykoz.oop;
+
+public class StudentName {
+
+    public void printNames(Student s1, Student s2) {
+
+        System.out.println("Their names are:");
+
+        System.out.println(s1.getName());
+
+        System.out.println(s2.getName());
+
+    }
+
+}
